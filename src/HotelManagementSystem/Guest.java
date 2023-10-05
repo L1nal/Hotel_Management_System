ae2586677ca9fc516f0683c5437160d9f877be71
Linal Mathila Bandara <linalmathilab@gmail.com>
2023-10-05 19:50:43 +0530

    Created getters and setters for the instance variables.

--- src/HotelManagementSystem/Guest.java
+++ src/HotelManagementSystem/Guest.java
@@ -14,5 +14,39 @@ public class Guest {
         this.discount = discount;
     }
 
+    public Guest(){
+        //Default Constructor.
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(int discount) {
+        this.discount = discount;
+    }
 }
