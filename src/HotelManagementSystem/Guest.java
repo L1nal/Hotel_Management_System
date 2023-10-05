997cfc8949cce7043550d3be22d90d5bbe483f40
Linal Mathila Bandara <linalmathilab@gmail.com>
2023-10-05 19:41:02 +0530

    Initiated the variables and constructed the constructor Guest.

---
+++ src/HotelManagementSystem/Guest.java
@@ -0,0 +1,18 @@
+package HotelManagementSystem;
+
+public class Guest {
+
+    int id;
+    String name;
+    String email;
+    int discount;
+
+    public Guest(int id, String name, String email, int discount) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.discount = discount;
+    }
+
+
+}
