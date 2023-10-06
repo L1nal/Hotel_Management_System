a3b8c9fc92731c27574f5c6378add5962f1f8e58
Linal Mathila Bandara <linalmathilab@gmail.com>
2023-10-06 19:12:18 +0530

    Created the Room.java class and initiated the variables and their constructors, getters and setters.

---
+++ src/HotelManagementSystem/Room.java
@@ -0,0 +1,83 @@
+package HotelManagementSystem;
+
+public class Room {
+    int id;
+    int floor;
+    int capacity;
+    String type;
+    String description;
+    double price;
+    boolean available;
+
+    public Room(int id, int floor, int capacity, String type, String description, double price, boolean available) {
+        this.id = id;
+        this.floor = floor;
+        this.capacity = capacity;
+        this.type = type;
+        this.description = description;
+        this.price = price;
+        this.available = available;
+    }
+
+    public Room() {
+        //default constructor
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getFloor() {
+        return floor;
+    }
+
+    public void setFloor(int floor) {
+        this.floor = floor;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+
+}
